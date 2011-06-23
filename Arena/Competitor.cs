3bf9f6fb2fd00231802619c13a2db55d48f4b316
Illidan Stormrage <jamadharma@gmail.com>
2011-06-23 11:33:56 +0600

    Arena

---
+++ Arena/Competitor.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: JD
+ * Date: 23.06.2011
+ * Time: 11:26
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using GodlySolver;
+
+namespace Arena
+{
+	class Competitor
+	{
+		public IStrategy Strategy;
+		public string Comment;
+		public int Score;
+		
+		public Competitor(IStrategy strategy, string comment, int score)
+		{
+			this.Strategy = strategy;
+			this.Comment = comment;
+			this.Score = score;
+		}
+	}
+}
---
+++ Arena/Program.cs
@@ -0,0 +1,59 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: JD
+ * Date: 23.06.2011
+ * Time: 11:26
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using GodlySolver;
+
+namespace Arena
+{
+	class Program
+	{
+
+		public static void Main(string[] args)
+		{
+			byte[,] map = new byte[,]{
+				{0,0,0,0,0,0,0,0},
+				{0,0,0,0,0,0,0,0},
+				{0,0,0,0,0,0,0,0},
+				{0,0,0,0,0,0,0,0},
+				{0,0,0,0,0,0,0,0},
+				{0,0,0,0,0,0,0,0},
+				{0,0,0,0,0,0,0,0},
+				{0,0,0,0,0,0,0,0},
+			};
+			
+			List<Competitor> competitors = new List<Competitor>(){
+				new Competitor(new GenericStrategy(1,1), "Greed level:1 moves:1     : ", 0),
+				new Competitor(new GenericStrategy(2,1), "Strategy level:2 moves:1  : ", 0),
+				new Competitor(new GenericStrategy(2,2), "Strategy level:2 moves:2  : ", 0),
+				new Competitor(new GenericStrategy(3,1), "Strategy level:3 moves:1  : ", 0),
+				new Competitor(new GenericStrategy(3,2), "Strategy level:3 moves:2  : ", 0),
+				new Competitor(new GenericStrategy(3,3), "Strategy level:3 moves:3  : ", 0),
+			};
+			
+			while(true)
+			{
+				
+				for(int i = 0; i<10 ; i++)
+				{
+					var board = new Board(map);
+					Game.CreateReal(board).CarefulRun();
+					foreach(var c in competitors)
+						c.Score += c.Strategy.Test(board.Copy());
+				}
+				
+				
+				foreach(var c in competitors)
+					Console.WriteLine(c.Comment+c.Score);
+				Console.WriteLine();
+			}
+
+		}
+	}
+}
